bdf10a41a2fb660738c4750fdda4a967456e1709
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2020-08-18 01:26:08 -0300

    Change message layout

--- lib/message_list_tile.dart
+++ lib/message_list_tile.dart
@@ -14,15 +14,38 @@ class MessageListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(message.imageUrl),
-      ),
-      title: Text(
-        message.user,
-        style: TextStyle(fontSize: 24),
-      ),
-      subtitle: _messageWidget(),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 18),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(message.imageUrl),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Text(
+                  message.user,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+              Container(width: 20, height: 20, child: Placeholder()),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 18),
+          child: _messageWidget(),
+        ),
+      ],
     );
   }
 
